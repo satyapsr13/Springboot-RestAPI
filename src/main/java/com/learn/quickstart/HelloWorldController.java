a16d179aeb2e26a5d282dc7a7588e2f89877b639
satyapsr13 <satyapsr13@gmail.com>
2024-06-14 14:13:55 +0530

    initial commit

---
+++ src/main/java/com/learn/quickstart/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.learn.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "hello")
+   public  String helloworld(
+
+   ){
+       return "Hello World";
+   }
+
+
+}
